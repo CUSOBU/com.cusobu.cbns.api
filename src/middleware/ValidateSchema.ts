a46e5a949a4693f412391013dc81949bcf13661d
Sandy Pérez Matamoros <sandypm007@gmail.com>
2023-07-13 15:46:40 -0300

    Variable rate (#5)
    * adding variable rate conf
    
    * fixing wrong provider on release
    
    * Using status as enum instad of el churre de yosmiel (#4)
    
    * Using status as enum instad of el churre de yosmiel
    
    * Adding uses for status enum
    
    ---------
    
    Co-authored-by: Jorge Yosmiel <jorgeyosmiel90@gmail.com>

--- src/middleware/ValidateSchema.ts
+++ src/middleware/ValidateSchema.ts
@@ -1,89 +1,103 @@
-import joi, { ObjectSchema } from 'joi';
-import { Response, Request, NextFunction } from 'express';
-import Logging from '../library/Logging';
-import IUser from '../interfaces/user';
-import IRemittance from '@src/interfaces/remittance';
-import { IRemittanceModel } from '@src/models/Remittance';
-import IBalance from '@src/interfaces/balance';
-import { IUserBalance } from '../models/Balance';
+import joi, { ObjectSchema } from "joi";
+import { Response, Request, NextFunction } from "express";
+import Logging from "../library/Logging";
+import IUser from "../interfaces/user";
+import IRemittance from "@src/interfaces/remittance";
+import { IRemittanceModel } from "@src/models/Remittance";
+import IBalance from "@src/interfaces/balance";
+import { IUserBalance } from "../models/Balance";
+import Status from "@src/common/Status";
 
 export const ValidateSchema = (schema: ObjectSchema) => {
-    return async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            await schema.validateAsync(req.body);
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await schema.validateAsync(req.body);
 
-            next();
-        } catch (error) {
-            Logging.error(error);
-            res.status(422).json({ error });
-        }
-    };
+      next();
+    } catch (error) {
+      Logging.error(error);
+      res.status(422).json({ error });
+    }
+  };
 };
 
 export const Schemas = {
-    user: {
-        create: joi.object<IUser>({
-            name: joi.string().required(),
-            email: joi.string().required().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'uy'] }}),
-            password: joi.string().required(),
-            role: joi.string().default('seller'),
-        }),
-        update: joi.object<IUser>({
-            name: joi.string().required(),
-            role: joi.string().default('seller')
-        })
-    },
-    remittance: {
-        create: joi.object<IRemittance>({
-            email: joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'uy'] }}),
-            full_name: joi.string(),
-           phone_number: joi.string(),
-           cardNumber: joi.string().required(),
-           remittance_amount: joi.number(),
-           remittance_currency: joi.string().required(),
-           budget_amount: joi.number().required(),
-           operation_cost: joi.number(),
-           budget_currency: joi.string().required(),
-           source_reference: joi.string(),
-           status: joi.string().default('pending'),
-           statusCode: joi.number().default(0),
-           webhook: joi.string(),
-           evidence: joi.string(),
-           details: joi.string()
-            
-        }),
-        update: joi.object<IRemittance>({
-            email: joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'uy'] }}),
-            status: joi.string(),
-            statusCode: joi.number(),
-            webhook: joi.string(),
-            evidence: joi.string(),
-            details: joi.string(),
-            provider: joi.string()
-        })
-    },
-    balance: {
-        create: joi.object<IUserBalance>({
-            email: joi.string().required().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'uy'] }}),
-            operational_price: joi.number(),
-            customer_price: joi.number(),
-            balance_usd: joi.number(),
-            balance_uyu: joi.number(),
-            operational_limit: joi.number(),
-            pre_paid: joi.boolean(),
-            allow_overlimit: joi.boolean(),
-            last_update: joi.date()
-        }),
-        update: joi.object<IUserBalance>({
-            email: joi.string().required().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'uy'] }}),
-            operational_price: joi.number(),
-            customer_price: joi.number(),
-            balance_usd: joi.number(),
-            balance_uyu: joi.number(),
-            operational_limit: joi.number(),
-            pre_paid: joi.boolean(),
-            allow_overlimit: joi.boolean(),
-            last_update: joi.date()
-        })
-    }
+  user: {
+    create: joi.object<IUser>({
+      name: joi.string().required(),
+      email: joi
+        .string()
+        .required()
+        .email({ minDomainSegments: 2, tlds: { allow: ["com", "net", "uy"] } }),
+      password: joi.string().required(),
+      role: joi.string().default("seller")
+    }),
+    update: joi.object<IUser>({
+      name: joi.string().required(),
+      role: joi.string().default("seller")
+    })
+  },
+  remittance: {
+    create: joi.object<IRemittance>({
+      email: joi
+        .string()
+        .email({ minDomainSegments: 2, tlds: { allow: ["com", "net", "uy"] } }),
+      full_name: joi.string(),
+      phone_number: joi.string(),
+      cardNumber: joi.string().required(),
+      remittance_amount: joi.number(),
+      remittance_currency: joi.string().required(),
+      budget_amount: joi.number().required(),
+      operation_cost: joi.number(),
+      budget_currency: joi.string().required(),
+      source_reference: joi.string(),
+      status: joi.string().default(Status.Pending),
+      statusCode: joi.number().default(0),
+      webhook: joi.string(),
+      evidence: joi.string(),
+      details: joi.string(),
+      remittance_rate: joi.number()
+    }),
+    update: joi.object<IRemittance>({
+      email: joi
+        .string()
+        .email({ minDomainSegments: 2, tlds: { allow: ["com", "net", "uy"] } }),
+      status: joi.string(),
+      statusCode: joi.number(),
+      webhook: joi.string(),
+      evidence: joi.string(),
+      details: joi.string(),
+      provider: joi.string()
+    })
+  },
+  balance: {
+    create: joi.object<IUserBalance>({
+      email: joi
+        .string()
+        .required()
+        .email({ minDomainSegments: 2, tlds: { allow: ["com", "net", "uy"] } }),
+      operational_price: joi.number(),
+      customer_price: joi.number(),
+      balance_usd: joi.number(),
+      balance_uyu: joi.number(),
+      operational_limit: joi.number(),
+      pre_paid: joi.boolean(),
+      allow_overlimit: joi.boolean(),
+      last_update: joi.date()
+    }),
+    update: joi.object<IUserBalance>({
+      email: joi
+        .string()
+        .required()
+        .email({ minDomainSegments: 2, tlds: { allow: ["com", "net", "uy"] } }),
+      operational_price: joi.number(),
+      customer_price: joi.number(),
+      balance_usd: joi.number(),
+      balance_uyu: joi.number(),
+      operational_limit: joi.number(),
+      pre_paid: joi.boolean(),
+      allow_overlimit: joi.boolean(),
+      last_update: joi.date()
+    })
+  }
 };
--- src/models/Remittance.ts
+++ src/models/Remittance.ts
@@ -1,41 +1,39 @@
-import mongoose, { Document, Schema } from 'mongoose';
-import IRemittance from '../interfaces/remittance';
-
+import mongoose, { Document, Schema } from "mongoose";
+import IRemittance from "../interfaces/remittance";
+import Status from "@src/common/Status";
 
 const schemaOptions = {
-    timestamps: true, // assigns createdAt and updatedAt fields to the schema
-    toJSON: { virtuals: true }, // allow to populate virtuals in every query
-    toObject: { virtuals: true },
-    versionKey: false
+  timestamps: true, // assigns createdAt and updatedAt fields to the schema
+  toJSON: { virtuals: true }, // allow to populate virtuals in every query
+  toObject: { virtuals: true },
+  versionKey: false
 };
 
 export interface IRemittanceModel extends IRemittance, Document {}
 
 const RemittanceSchema: Schema = new Schema(
-    {
-        identifier: { type: Number, required: true, unique: true },
-        email: { type: String, required: true },
-        full_name: { type: String, required: true },
-        phone_number: { type: String, required: true },
-        cardNumber: { type: String, required: true },   
-        remittance_amount: { type: Number, required: true },
-        remittance_currency: { type: String, required: true },
-        budget_amount: { type: Number, required: true },
-        operation_cost: { type: Number, required: true },
-        budget_currency: { type: String, required: true },
-        source_reference: { type: String },
-        status: { type: String, required: true, default: 'pending' },
-        statusCode: { type: String, required: true, default: '0' },
-        webhook: { type: String },
-        evidence: { type: String },
-        details: { type: String },
-        provider: { type: String }
-    },
-    schemaOptions
+  {
+    identifier: { type: Number, required: true, unique: true },
+    email: { type: String, required: true },
+    full_name: { type: String, required: true },
+    phone_number: { type: String, required: true },
+    cardNumber: { type: String, required: true },
+    remittance_amount: { type: Number, required: true },
+    remittance_currency: { type: String, required: true },
+    budget_amount: { type: Number, required: true },
+    operation_cost: { type: Number, required: true },
+    budget_currency: { type: String, required: true },
+    source_reference: { type: String },
+    status: { type: String, required: true, default: Status.Pending },
+    statusCode: { type: String, required: true, default: "0" },
+    webhook: { type: String },
+    evidence: { type: String },
+    details: { type: String },
+    provider: { type: String }
+  },
+  schemaOptions
 );
 
-export default mongoose.model<IRemittanceModel>('Remittance', RemittanceSchema);
+export default mongoose.model<IRemittanceModel>("Remittance", RemittanceSchema);
 
-export {
-    IRemittance
-}
+export { IRemittance };
